3792cf6a4fa2ed142e89fa01ccb3acdaf6630b7d
dtiendzai123 <hanknguyen2112@gmail.com>
2025-07-19 22:14:43 +0700

    draglockbone.js

--- draglockbone.js
+++ draglockbone.js
@@ -1,3 +1,7 @@
+const GamePackages = {
+  GamePackage1: "com.dts.freefireth",
+  GamePackage2: "com.dts.freefiremax"
+};
 class Vector3 {
   constructor(x = 0, y = 0, z = 0) { this.x = x; this.y = y; this.z = z; }
   add(v) { return new Vector3(this.x + v.x, this.y + v.y, this.z + v.z); }
